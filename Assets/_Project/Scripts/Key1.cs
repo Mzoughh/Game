8cb87a9f2cd6665aada19d8157d050b15c7864c5
elliot COLE <elliot.cole@telecom-sudparis.eu>
2023-10-22 13:45:21 +0200

    game design and logic save 1

---
+++ Assets/_Project/Scripts/Key1.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Key1 : MonoBehaviour
+{
+    public GameObject key2;            // Drag the key2 object from your scene here.
+    public GameObject[] boxes;         // Drag the 3 boxes from your scene here.
+
+    private void Start()
+    {
+        // Ensure key2 and the boxes start as inactive.
+        key2.SetActive(false);
+
+        foreach (GameObject box in boxes)
+        {
+            box.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Assuming your player object has a tag named "Player".
+        {
+            if (this.CompareTag("Key1"))
+            {
+                CollectKey1();
+            }
+
+            // Hide the key after collection.
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    void CollectKey1()
+    {
+        // Activate key2.
+        key2.SetActive(true);
+
+        // Activate the 3 boxes.
+        foreach (GameObject box in boxes)
+        {
+            box.SetActive(true);
+        }
+    }
+}
+
---
+++ Assets/_Project/Scripts/PlayerGrab.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;  // This is the namespace for TextMesh Pro.
+
+public class PlayerGrab : MonoBehaviour
+{
+    public float grabDistance = 2.0f;
+    public TextMeshProUGUI instructionText;  // This is the TMP text component for UI.
+
+    private GameObject currentGrabbableObject;
+    private bool isHoldingObject = false;
+
+    void Start()
+    {
+        instructionText.text = "";
+    }
+
+    void Update()
+    {
+        CheckForGrabbableObject();
+        HandleGrabRelease();
+    }
+
+    private void CheckForGrabbableObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, grabDistance))
+        {
+            if (hit.transform.CompareTag("Grabbable") && !isHoldingObject)
+            {
+                instructionText.text = "Press SHIFT to grab the object.";
+                currentGrabbableObject = hit.transform.gameObject;
+            }
+        }
+        else
+        {
+            instructionText.text = "";
+            currentGrabbableObject = null;
+        }
+    }
+
+    private void HandleGrabRelease()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && currentGrabbableObject)
+        {
+            if (isHoldingObject)
+            {
+                // Release the object
+                currentGrabbableObject.transform.SetParent(null);
+                isHoldingObject = false;
+            }
+            else
+            {
+                // Grab the object
+                currentGrabbableObject.transform.SetParent(this.transform);
+                isHoldingObject = true;
+            }
+        }
+    }
+}
